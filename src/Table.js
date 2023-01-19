a59e5ffba3b841e5121a7c4241276d9b7ef93360
giuliarappo <rappogiulia@gmail.com>
2023-01-19 11:30:00 -0600

    implementa tabella con dati json

--- src/Table.js
+++ src/Table.js
@@ -1,55 +1,69 @@
 import React, { Component } from 'react';
+import fateChart from './resurcess/dati.json';
 import './Table.css';
 
-class Table extends Component {
-  render() {
+
+
+//Selezionare la riga corrispondente a quella selezionata(passando una props? o lo state)
+
+
+function TableWithJsonData() {
+  console.log("prova");
+  const DisplayData = fateChart.map(
+    (info, index) => {
+      return(
+        <tr>
+          <th>{info.probability}</th>
+          {info.value.map((values) => {
+            console.log("yesNo: " + values.yesNo)
+            return(
+              <th>
+                <span>
+                  {values.exceptionalYes}
+                </span>
+                <span>
+                  {values.yesNo}
+                </span>
+                <span>
+                  {values.exceptionalNo}
+                </span>
+                
+              </th>
+            )
+          })}
+        </tr>
+        
+      )
+    }
+  )
+
+ 
+    console.log(DisplayData);
     return (
-      <div className='centered'>
+      <div class="centered">
         <table>
-        <thead>
-          <tr>
-            <th>Caos</th>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
-            <th>4</th>
-            <th>5</th>
-            <th>6</th>
-            <th>7</th>
-            <th>8</th>
-            <th>9</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Impossible</td>
-            <td>0<span>-20</span>77</td>
-            <td>0<span>0</span>81</td>
-            <td>0<span>0</span>81</td>
-            <td>1<span>5</span>82</td>
-            <td>1<span>5</span>82</td>
-            <td>2<span>10</span>83</td>
-            <td>3<span>15</span>84</td>
-            <td>5<span>25</span>86</td>
-            <td>10<span>50</span>91</td>
-          </tr>
-          <tr>
-            <td>No Way</td>
-            <td>0<span>0</span>81</td>
-            <td>1<span>5</span>82</td>
-            <td>1<span>5</span>82</td>
-            <td>2<span>10</span>83</td>
-            <td>3<span>15</span>84</td>
-            <td>5<span>25</span>86</td>
-            <td>7<span>35</span>88</td>
-            <td>10<span>50</span>91</td>
-            <td>10<span>75</span>96</td>
-          </tr>
-        </tbody>
-      </table>
+          <thead>
+            <tr>
+              <th>Probability</th>
+              <th>1</th>
+              <th>2</th>
+              <th>3</th>
+              <th>4</th>
+              <th>5</th>
+              <th>6</th>
+              <th>7</th>
+              <th>8</th>
+              <th>9</th>
+            </tr>
+          </thead>
+          {DisplayData}
+        </table>
       </div>
-    )
-  }
+    );
+  
 }
 
-export default Table
+
+ 
+export default TableWithJsonData;
+
