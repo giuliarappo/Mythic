864539f9b69030d6dd83b9e3c480b5af94ed4323
giuliarappo <rappogiulia@gmail.com>
2023-01-20 07:45:54 -0600

    refactoring

---
+++ src/MenuScelta.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import fateChart from './resurcess/dati.json';
+
+
+const DisplayData = fateChart.map(
+    (probability) => {
+        console.log(probability)
+
+        return(
+            
+            <option>{probability.probability}</option>
+            
+        )
+    }
+)
+
+class MenuScelta extends Component {
+    
+
+    handleChange = (event) => {
+        this.props.probabilitaScelta(event.target.value);
+        event.preventDefault();
+    }
+
+    render() { 
+        return (
+            <select onChange={this.handleChange}>
+                <option></option>
+                {DisplayData}
+            </select>
+        );
+    }
+}
+ 
+export default MenuScelta;
