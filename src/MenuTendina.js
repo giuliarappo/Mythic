b99f3d2353acce68feffed237c6c8c788c284008
giuliarappo <rappogiulia@gmail.com>
2023-01-20 01:52:55 -0600

    prende dati dal json

--- src/MenuTendina.js
+++ src/MenuTendina.js
@@ -1,3 +1,20 @@
+import React, { Component } from 'react';
+import fateChart from './resurcess/dati.json';
+import CalcolaRisposta from './CalcolaRisposta';
+
+const DisplayData = fateChart.map(
+    (probability) => {
+        console.log(probability)
+
+        return(
+            
+            <option>{probability.probability}</option>
+            
+        )
+    }
+)
+
+
 class MenuTendina extends Component {
     constructor(props) {
         super(props);
@@ -12,23 +29,12 @@ class MenuTendina extends Component {
     render() { 
         return (
             <div>
-                <select onChange={this.handleChange}>
-                <option></option>
-                <option value="impossible">impossible</option>
-                <option value="noWay">noWay</option>
-                <option value="veryUnlikely">veryUnlikely</option>
-                <option value="unlikey">unlikely</option>
-                <option value="5050">50_50</option>
-                <option value="somewhatLikely">somewhatLikely</option>
-                <option value="likely">likely</option>
-                <option value="veryLikely">veryLikely</option>
-                <option value="nearSureThing">nearSureThing</option>
-                <option value="aSureThing">aSureThing</option>
-                <option value="hasToBe">hasToBe</option>
+                <select>
+                    <option></option>
+                    {DisplayData}
                 </select>
-                <h2>{this.state.selecteOption}</h2>
+                <CalcolaRisposta selecteOption={this.state.selecteOption}/>
             </div>
-            
         );
     }
 }
